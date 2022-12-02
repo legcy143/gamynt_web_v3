edaa3b5490e08f3a5de310661de039e6740b1155
legcy <legcy143@gmail.com>
2022-12-02 08:01:39 +0530

    adding top featured club v1

--- Page/Home/Topfeaturedclubs.jsx
+++ Page/Home/Topfeaturedclubs.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Image from "next/image"
 import style from "./styles/homeitem.module.scss"
 
 const Topfeaturedclubs = () => {
   return (
-    <div>Topfeaturedclubs</div>
+  <main className={style.top_featured_container}>
+    <header>
+      <h2>top featured clubs</h2>
+    </header>
+
+      <section className={style.crausal_container}>
+
+        <div className={style.club_item}>
+        <Image src="/images/freefire.jpeg" alt="" width={200} height={200}/>
+        <div>
+          <h1>bull ka group</h1>
+          <p>250</p>
+          <button>join</button>
+        </div>
+        </div>
+        
+        <div className={style.club_item}>
+        <Image src="/images/freefire.jpeg" alt="" width={200} height={200}/>
+        <div>
+          <h1>bull ka group</h1>
+          <p>250</p>
+          <button>join</button>
+        </div>
+        </div>
+
+        <div className={style.club_item}>
+        <Image src="/images/freefire.jpeg" alt="" width={200} height={200}/>
+        <div>
+          <h1>bull ka group</h1>
+          <p>250</p>
+          <button>join</button>
+        </div>
+        </div>
+
+      </section>
+
+  </main>
   )
 }
 
