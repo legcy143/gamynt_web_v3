6234d0f646257957dcf188112e008955f4543927
Siddhant <94226898+S1D007@users.noreply.github.com>
2022-12-03 07:48:04 +0000

    Fixes done

--- Page/Home/Topfeaturedtournament.jsx
+++ Page/Home/Topfeaturedtournament.jsx
@@ -14,18 +14,18 @@ const Topfeaturedtournament = () => {
 
     <div className={style.tournament_item}>
       <div className={style.image_space}>
-        <Image src="/images/freefire.jpeg" alt="" width={200} height={200}/>
+        <img src="/images/freefire.jpeg" alt="" width={200} height={200}/>
         <span>2 thu 2:50 pm</span>
       </div>
       <div className={style.admin_detail}>
-        <Image src="/images/freefire.jpeg" alt="" width={50} height={50}/>
+        <img src="/images/freefire.jpeg" alt="" width={50} height={50}/>
         <h3>gamynt admin</h3>
       </div>
         <div className={style.tournament_info}>
         
-       <p> <Image src="/images/freefire.jpeg" alt="" width={25} height={25}/>  free fire</p>
-       <p><Image src="/util/prize.webp" alt="" width={35} height={25}/>   $ 250</p>
-       <p> <Image src="/util/member.png" alt="" width={35} height={30}/> member slot</p>
+       <p> <img src="/images/freefire.jpeg" alt="" width={25} height={25}/>  free fire</p>
+       <p><img src="/util/prize.webp" alt="" width={35} height={35}/>₹ 250</p>
+       <p> <img src="/util/member.png" alt="" width={35} height={35}/>7/14</p>
        <p><GroupIcon/> solo</p>
         </div>
         <button>join tournament</button>
