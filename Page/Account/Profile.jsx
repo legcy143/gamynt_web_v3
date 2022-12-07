0b441fe71cdfb901716892af46f600d59543bd6d
legcy <legcy143@gmail.com>
2022-12-07 23:26:15 +0530

    profile v2

--- Page/Account/Profile.jsx
+++ Page/Account/Profile.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Profilecard from './component/Profilecard'
 import style from "./styles/profile.module.scss"
+import FriendsIcon from '@mui/icons-material/Diversity3';
 
 const Profile = () => {
   return (
     <main className={style.profile_main}>
         <Profilecard/>
+        <nav className={style.nav}>
+          <ul className={style.active_link}>
+            <FriendsIcon/>
+            <li>friends</li>
+          </ul>
+          <ul>
+            <FriendsIcon/>
+            <li>clubs</li>
+          </ul>
+          <ul>
+            <FriendsIcon/>
+            <li>post</li>
+          </ul>
+          <ul>
+            <FriendsIcon/>
+            <li>shorts</li>
+          </ul>
+        </nav>
     </main>
   )
 }
