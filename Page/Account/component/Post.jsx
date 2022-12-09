eb16f511de5aab355445c1416474da0620b82564
legcy <legcy143@gmail.com>
2022-12-10 00:28:45 +0530

    profile added post and shorts

--- Page/Account/component/Post.jsx
+++ Page/Account/component/Post.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import style from "../styles/profileitem.module.scss"
 
 const Post = () => {
   return (
-    <div>Post</div>
+    <main className={style.post_container}>
+      <section className={style.post_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <div>
+          <p>22k views</p>
+          <p>22k views</p>
+        </div>
+      </section>
+      <section className={style.post_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <div>
+          <p>22k views</p>
+          <p>22k views</p>
+        </div>
+      </section>
+      <section className={style.post_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <div>
+          <p>22k views</p>
+          <p>22k views</p>
+        </div>
+      </section>
+      <section className={style.post_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <div>
+          <p>22k views</p>
+          <p>22k views</p>
+        </div>
+      </section>
+      <section className={style.post_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <div>
+          <p>22k views</p>
+          <p>22k views</p>
+        </div>
+      </section>
+   
+    </main>
   )
 }
 
--- Page/Account/component/Shorts.jsx
+++ Page/Account/component/Shorts.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import style from "../styles/profileitem.module.scss"
+import MovieFilterIcon from '@mui/icons-material/MovieFilter';
 
 const Shorts = () => {
   return (
-    <div>Shorts</div>
+    <main className={style.shorts_container}>
+      <section className={style.shorts_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <MovieFilterIcon />
+        <div>
+          <p>22k views</p>
+          <p>22k like</p>
+        </div>
+      </section>
+      <section className={style.shorts_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <MovieFilterIcon />
+        <div>
+          <p>22k views</p>
+          <p>22k like</p>
+        </div>
+      </section>
+      <section className={style.shorts_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <MovieFilterIcon />
+        <div>
+          <p>22k views</p>
+          <p>22k like</p>
+        </div>
+      </section>
+      <section className={style.shorts_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <MovieFilterIcon />
+        <div>
+          <p>22k views</p>
+          <p>22k like</p>
+        </div>
+      </section>
+      <section className={style.shorts_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <MovieFilterIcon />
+        <div>
+          <p>22k views</p>
+          <p>22k like</p>
+        </div>
+      </section>
+      <section className={style.shorts_box}>
+        <img src="/images/freefire.jpeg" alt="" />
+        <MovieFilterIcon />
+        <div>
+          <p>22k views</p>
+          <p>22k like</p>
+        </div>
+      </section>
+    </main>
   )
 }
 
