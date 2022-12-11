9bfa640811e520ec910010adbade778061269909
legcy <legcy143@gmail.com>
2022-12-11 17:27:20 +0530

    notification v1

--- components/Navbar/TopNavBar.jsx
+++ components/Navbar/TopNavBar.jsx
@@ -4,45 +4,57 @@ import style from "./TopNavBar.module.scss"
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { useRouter } from 'next/router';
+import Modal from '@mui/material/Modal';
+import Notifiaction from '../Notification/Notifiaction';
 
 const TopNavbar = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   let router = useRouter();
   let path = router.pathname;
-  
+
   return (
     <nav className={style.top_navbar}>
 
       <section className={style.page_name}>
-        <h2>{path.slice(1,15)}</h2>
-        <h1>{path==="/"? "home" :path.slice(1,15)}</h1>
+        <h2>{path.slice(1, 15)}</h2>
+        <h1>{path === "/" ? "home" : path.slice(1, 15)}</h1>
       </section>
 
-        <section className={style.search_box}>
-        <input type="text" placeholder='search..'/>
-        <SearchIcon/>
-        </section>
+      <section className={style.search_box}>
+        <input type="text" placeholder='search..' />
+        <SearchIcon />
+      </section>
 
-        <section className={style.profile_section}>
-          <div className={style.coin_diamond_container}>
-            <div className={style.coin_nav}>
-              <p>23</p>
-              <input type="range" name="points" min="0" max="100" value={0}/> 
-              </div>
-            <div className={style.coin_nav}>
-              <p>23</p>
-              <input type="range" name="points" min="0" max="100" value={0}/> 
-              </div>
+      <section className={style.profile_section}>
+        <div className={style.coin_diamond_container}>
+          <div className={style.coin_nav}>
+            <p>23</p>
+            <input type="range" name="points" min="0" max="100" value={0} />
           </div>
-
-          <div className={style.notification}>
-            <NotificationsIcon/>
-            <p>34</p>
+          <div className={style.coin_nav}>
+            <p>23</p>
+            <input type="range" name="points" min="0" max="100" value={0} />
           </div>
+        </div>
 
-            <div className={style.profile_icon}>
-              <h1>p</h1>
-            </div>
-        </section>
+        <div className={style.notification} onClick={()=>{handleOpen()}}>
+          <NotificationsIcon />
+          <p>34</p>
+        </div>
+
+        <div className={style.profile_icon}>
+          <h1>p</h1>
+        </div>
+      </section>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        className="model"
+      >
+        <Notifiaction/>
+      </Modal>
     </nav>
   )
 }
---
+++ components/Notification/Notifiaction.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import style from "./notify.module.scss"
+
+
+const Notifiaction = () => {
+  return (
+    <main className={style.notification_main_container}>
+        <header>
+            <h1>notification</h1>
+        </header>
+        <section className={style.notification}>
+            <img src="/images/freefire.jpeg" alt="img" />
+            <div>
+                <p>hello sid how are</p>
+                <span>12:12 pm</span>
+            </div>
+        </section>
+        <section className={style.notification}>
+            <img src="/images/freefire.jpeg" alt="img" />
+            <div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas delectus et nulla unde aspernatur pariatur corrupti. Minus, blanditiis! Explicabo quasi enim doloribus similique rerum sunt repudiandae facilis inventore suscipit voluptas!</p>
+                <span>12:12 pm</span>
+            </div>
+        </section>
+        <section className={style.notification}>
+            <img src="/images/freefire.jpeg" alt="img" />
+            <div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <span>12:12 pm</span>
+            </div>
+        </section>
+    </main>
+  )
+}
+
+export default Notifiaction
