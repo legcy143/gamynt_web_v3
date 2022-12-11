e480d39f8874373dfc8802ee8e7e4ec826f49108
legcy <legcy143@gmail.com>
2022-12-11 16:53:05 +0530

    authentication v4

--- Page/Account/Login.jsx
+++ Page/Account/Login.jsx
@@ -1,30 +1,41 @@
-import React , {Fragment, useState} from 'react'
+import React, { Fragment, useState, useRef } from 'react'
 import style from "./styles/auth.module.scss"
 import PhoneIcon from '@mui/icons-material/Phone';
 
 const Login = () => {
   let btntxt = "next";
-  const [phoneno, setphoneno] = useState(0)
-  const formsubmit = (e)=>{
-    e.preventDefault()
+  let inptdiv = useRef()
+  let otpdiv = useRef()
+  let phonenumber = useRef()
+
+  const formsubmit = (e) => {
+    e.preventDefault();
+    if (phonenumber.current.value.length === 0) {
+      inptdiv.current.setAttribute("class", style.nexttoggle);
+      otpdiv.current.setAttribute("class", style.otp_box)
+    }
+    else {
+      console.log("please enter correct number")
+    }
   }
   return (
-    <>
-    <form className={style.auth_form} onSubmit={formsubmit}>
-    <div className={style.form_input_box}>
-          <PhoneIcon/>
-          <input type="text" placeholder='phone number'/>
-      </div>
-      <div className={style.otp_box}>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
+    <Fragment>
+      <form className={style.auth_form} onSubmit={formsubmit}>
+        <div className={`${style.form_input_box}`} ref={inptdiv}>
+          <PhoneIcon />
+          <input type="text" placeholder='phone number'
+            ref={phonenumber} />
+        </div>
+        <div className={`${style.nexttoggle}`} ref={otpdiv}>
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
         </div>
         <button>{btntxt}</button>
-    </form>
-    </>
+      </form>
+    </Fragment>
   )
 }
 
--- Page/Account/Signup.jsx
+++ Page/Account/Signup.jsx
@@ -1,31 +1,40 @@
-import React , {Fragment} from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import style from "./styles/auth.module.scss"
-import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
-import LockIcon from '@mui/icons-material/Https';
 import PhoneIcon from '@mui/icons-material/Phone';
-import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const Signup = () => {
   let btntxt = "next";
-  const formsubmit = (e)=>{
-    e.preventDefault()
+  let inptdiv = useRef()
+  let otpdiv = useRef()
+  let phonenumber = useRef()
+
+
+  const formsubmit = (e) => {
+    e.preventDefault();
+    // inptdiv
+    if (phonenumber.current.value.length === 0) {
+      inptdiv.current.setAttribute("class", style.nexttoggle);
+      otpdiv.current.setAttribute("class", style.otp_box)
+    }
+    else {
+      console.log("please enter correct number")
+    }
+    console.log(phonenumber.current.value)
   }
   return (
     <Fragment>
       <form className={style.auth_form} onSubmit={formsubmit}>
-
-      <div className={style.form_input_box}>
-          <PhoneIcon/>
-          <input type="text" placeholder='phone number'/>
+        <div className={`${style.form_input_box}`} ref={inptdiv}>
+          <PhoneIcon />
+          <input type="text" placeholder='phone number'
+            ref={phonenumber} />
         </div>
-
-        <div className={style.otp_box}>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
-          <input type="text" placeholder='0'/>
+        <div className={`${style.nexttoggle}`} ref={otpdiv}>
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
+          <input type="text" placeholder='0' />
         </div>
         <button>{btntxt}</button>
       </form>
