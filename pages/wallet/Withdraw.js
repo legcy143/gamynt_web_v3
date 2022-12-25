7043d76e9ad4abcfd846f9eba4952980c299a955
legcy <legcy143@gmail.com>
2022-12-25 12:39:44 +0530

    account section v2

--- pages/wallet/Withdraw.js
+++ pages/wallet/Withdraw.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import Walletnav from '../../Page/Wallet/Walletnav'
-import style from "./styles/wallet.module.scss"
+import wallet from "./styles/wallet.module.scss"
+import style from "./styles/walletitem.module.scss"
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 
 const Withdraw = () => {
   return (
-    <main className={style.moneyaction_container_main}>
+    <main className={wallet.moneyaction_container_main}>
     <Walletnav tittle={"withdraw"}/>
-<h1>with draw</h1>
+<header className={style.header_message}>
+<small>you can directly withdraw your money to bank account</small>
+</header>
+
+<section className={style.withdraw_container}>
+  <div className={style.withdrwa_acc_input}>
+    <span>account number</span>
+    <input type="text" placeholder='account number'/>
+  </div>
+  <div className={style.withdraw_coin_detail}>
+  <p>available ammout : $3455</p>
+  </div>
+  <div className={style.withdraw_input}>
+    <span>enter amout</span>
+    <div>
+      <CurrencyRupeeIcon/>
+    <input type="text" placeholder='amount'/>
+    </div>
+  </div>
+  <button className={style.withdrwa_btn}>withdraw</button>
+</section>
   </main>
   )
 }
