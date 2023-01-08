7430c4a8b459ca8538c8394bfd7314d74e3b496a
legcy <legcy143@gmail.com>
2023-01-08 16:28:22 +0530

    dashboard v5

--- Page/dashboard/ClubAnyalystic.jsx
+++ Page/dashboard/ClubAnyalystic.jsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import style from "./styles/clubanyalistic.module.scss"
+import GamesIcon from '@mui/icons-material/Games';
 
 const ClubAnyalystic = () => {
   return (
     <main className={style.main_container}>
 
         <section className={style.member_status}>
+            <header>Tournament</header>
             <div className={style.container}>
-                <h2>total member</h2>
+                <h2><GamesIcon/> tournament</h2>
                 <p>450</p>
             </div>
             <div className={style.container}>
-                <h2>active memebr</h2>
+                <h2><GamesIcon/> tournament</h2>
                 <p>450</p>
             </div>
             <div className={style.container}>
-                <h2>tournament active</h2>
-                <p>12</p>
+                <h2><GamesIcon/> tournament</h2>
+                <p>450</p>
             </div>
             <div className={style.container}>
-                <h2>tournament completed</h2>
-                <p>250</p>
+                <h2><GamesIcon/> tournament</h2>
+                <p>450</p>
+            </div>
+        </section>
+
+        <section className={style.member_status}>
+            <header>club</header>
+            <div className={style.container}>
+                <h2><GamesIcon/> tournament</h2>
+                <p>450</p>
+            </div>
+            <div className={style.container}>
+                <h2><GamesIcon/> tournament</h2>
+                <p>450</p>
+            </div>
+            <div className={style.container}>
+                <h2><GamesIcon/> tournament</h2>
+                <p>450</p>
+            </div>
+            <div className={style.container}>
+                <h2><GamesIcon/> tournament</h2>
+                <p>450</p>
             </div>
         </section>
 
--- pages/club/dashboard.js
+++ pages/club/dashboard.js
@@ -4,8 +4,11 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import style from "./styles/dashboard.module.scss"
 import ClubAnyalystic from "../../Page/dashboard/ClubAnyalystic"
 import MenuIcon from '@mui/icons-material/Menu';
+import { useRouter } from 'next/router';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const Dashboard = () => {
+    let router = useRouter()
     const [sidenav, setsidenav] = useState(style.close_nav)
     // const [sidenav, setsidenav] = useState(style.main_nav)
     // let todaydate = new Date().toLocaleString()
@@ -16,8 +19,9 @@ const Dashboard = () => {
   return (
       <main className={style.dashboard_main}>
         <nav className={sidenav}>
-            <div className={style.close_nav_icon}  onClick={()=>{togglenav()}} >
-        <MenuIcon/>
+            <div className={style.close_nav_icon}>
+        <ArrowBackIcon onClick={()=>{router.back(-1)}} />
+        <MenuIcon onClick={()=>{togglenav()}} />
             </div>
             <header>
                 <img src="/images/freefire.jpeg" alt="" />
@@ -29,11 +33,11 @@ const Dashboard = () => {
             </ul>
             <ul>
                 <DashboardIcon/>
-                <li>dashboard</li>
+                <li>tournament</li>
             </ul>
             <ul>
                 <DashboardIcon/>
-                <li>dashboard</li>
+                <li>club</li>
             </ul>
         </nav>
         <section className={style.dashboard_item_container}>
