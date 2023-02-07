d3c1c7d24277148c27313ee334482c8b49c79fde
legcy <legcy143@gmail.com>
2023-02-07 18:04:25 +0530

    --subscription pgae ui

--- pages/streams/stream.js
+++ pages/streams/stream.js
@@ -5,128 +5,128 @@ import { useRouter } from 'next/router';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ShareIcon from '@mui/icons-material/Share';
 import SendIcon from '@mui/icons-material/Send';
+import WarningIcon from '@mui/icons-material/Warning';
 
 const stream = () => {
-    let router = new useRouter()
-    const sharefunc = ()=>{
-      const shareurl = window.location.href
-      console.log("share func" , shareurl)
+  let router = new useRouter()
+  const sharefunc = () => {
+    const shareurl = window.location.href
+    console.log("share func", shareurl)
 
-      let shareData = {
-        title: 'gamynt',
-        text: 'from gamynt',
-        url: window.location.href,
-      }
-     
-if (!navigator.canShare) {
-  console.log("not supported this browser")
-}
-else if (navigator.canShare(shareData)) {
-  console.log("work fine")
-  navigator.share(shareData)
-} else {
-  console.log("something wrong")
-}
+    let shareData = {
+      title: 'gamynt',
+      text: "let's watch stream together in gamynt",
+      url: window.location.href,
     }
+
+    if (!navigator.canShare) {
+      console.log("not supported this browser")
+    }
+    else if (navigator.canShare(shareData)) {
+      navigator.share(shareData)
+    } else {
+      console.log("something wrong")
+    }
+  }
   return (
     <>
-    <nav className={style.nav}>
+      <nav className={style.nav}>
         <section>
-            <button onClick={()=>router.back(-1)} > <ArrowBackIcon/> back</button>
+          <button onClick={() => router.back(-1)} > <ArrowBackIcon /> back</button>
         </section>
         <section>
-            any thing else
+          <button className={style.report}><WarningIcon/> report</button>
         </section>
-    </nav>
-    {/* vedio streaming parts */}
-    <main className={style.stream_container}>
-    {/* vedio streaming with vedio and tittle info */}
-      <section className={style.stream_section}>
-        {/* vedio container  */}
-        <div className={style.vedio_container}>
-          <img src="/images/fortnite.jpeg" alt="img" />
-          <section className={style.vedio_actions_container}>
-            <div className={style.bottom_action}>
-              <input type="range" name="" id="" />
-              <span>live</span>
-            </div>
-          </section>
-        </div>
-        {/* vedio container  */}
-        <div className={style.stream_details}>
-          <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni assumenda aspernatur voluptates voluptas voluptatibus</h2>
-          <div className={style.stream_more_option}>
-            <button><ThumbUpIcon/></button>
-            <button><ThumbUpIcon style={{transform:"rotate(180deg)"}}/></button>
-            <button onClick={()=>{sharefunc()}}><ShareIcon/></button>
+      </nav>
+      {/* vedio streaming parts */}
+      <main className={style.stream_container}>
+        {/* vedio streaming with vedio and tittle info */}
+        <section className={style.stream_section}>
+          {/* vedio container  */}
+          <div className={style.vedio_container}>
+            <img src="/images/fortnite.jpeg" alt="img" />
+            <section className={style.vedio_actions_container}>
+              <div className={style.bottom_action}>
+                <input type="range" name="" id="" />
+                <span>live</span>
+              </div>
+            </section>
           </div>
-          <div className={style.streamer_owner_info}>
-          <img src="/images/fortnite.jpeg" alt="img" />
-          <p>user name</p>
-          <button>follow</button>
-          </div>
-        </div>
-
-      </section>
-
-    {/* liove chat and many more */}
-      <section className={style.stream_feature_section}>
-        <nav className={style.stream_feature_nav}>
-          <li>live chat</li>
-        </nav>
-        <div className={style.livechat_container}>
-
-          <div className={style.live_user}>
-            <div>
-            <img src="/images/fortnite.jpeg" alt="img" />
-              <small>user name</small>
+          {/* vedio container  */}
+          <div className={style.stream_details}>
+            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni assumenda aspernatur voluptates voluptas voluptatibus</h2>
+            <div className={style.stream_more_option}>
+              <button><ThumbUpIcon /></button>
+              <button><ThumbUpIcon style={{ transform: "rotate(180deg)" }} /></button>
+              <button onClick={() => { sharefunc() }}><ShareIcon /></button>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium 
-            </p>
-            </div>
-          <div className={style.live_user}>
-            <div>
-            <img src="/images/fortnite.jpeg" alt="img" />
-              <small>user name</small>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium 
-            </p>
-            </div>
-          <div className={style.live_user}>
-            <div>
-            <img src="/images/fortnite.jpeg" alt="img" />
-              <small>user name</small>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium 
-            </p>
-            </div>
-          <div className={style.live_user}>
-            <div>
-            <img src="/images/fortnite.jpeg" alt="img" />
-              <small>user name</small>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium 
-            </p>
-            </div>
-
-          <div className={style.send_live_chat}>
-            <div>
-            <img src="/images/fortnite.jpeg" alt="img" />
+            <div className={style.streamer_owner_info}>
+              <img src="/images/fortnite.jpeg" alt="img" />
               <p>user name</p>
+              <button>follow</button>
             </div>
-            <label>
-            <textarea type="text" placeholder='type something ..' />
-            <SendIcon/>
-            </label>
           </div>
-        </div>
 
-      </section>
-    </main>
+        </section>
+
+        {/* liove chat and many more */}
+        <section className={style.stream_feature_section}>
+          <nav className={style.stream_feature_nav}>
+            <li>live chat</li>
+          </nav>
+          <div className={style.livechat_container}>
+
+            <div className={style.live_user}>
+              <div>
+                <img src="/images/fortnite.jpeg" alt="img" />
+                <small>user name</small>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium
+              </p>
+            </div>
+            <div className={style.live_user}>
+              <div>
+                <img src="/images/fortnite.jpeg" alt="img" />
+                <small>user name</small>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium
+              </p>
+            </div>
+            <div className={style.live_user}>
+              <div>
+                <img src="/images/fortnite.jpeg" alt="img" />
+                <small>user name</small>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium
+              </p>
+            </div>
+            <div className={style.live_user}>
+              <div>
+                <img src="/images/fortnite.jpeg" alt="img" />
+                <small>user name</small>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error voluptas quam rem praesentium
+              </p>
+            </div>
+
+            <div className={style.send_live_chat}>
+              <div>
+                <img src="/images/fortnite.jpeg" alt="img" />
+                <p>user name</p>
+              </div>
+              <label>
+                <textarea type="text" placeholder='type something ..' />
+                <SendIcon />
+              </label>
+            </div>
+          </div>
+
+        </section>
+      </main>
     </>
   )
 }
--- pages/subscription.js
+++ pages/subscription.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import style from "./styles/subscription.module.scss"
+import SubIcon from '@mui/icons-material/GridGoldenratio';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Subscription = () => {
   return (
-    <div>Subscription</div>
+    <main className={style.subscription_main_container}>
+      <ToastContainer style={{width:"min(20rem , 70%)"}}/>
+      <section className={style.Subscription_section}>
+
+        <div className={style.sub_card}>
+          <header>
+            <h1>
+<SubIcon/> basic
+            </h1>
+          </header>
+          <ul>
+          <li>no ads</li>
+          <li>no ads</li>
+          <li>no ads</li>
+          <li>free prime message</li>
+          </ul>
+          <footer>
+            <h1>$10 <span>/ month</span></h1>
+            <button onClick={()=>toast.error("something went wrong")}>choose</button>
+          </footer>
+        </div>
+
+        <div className={style.sub_card}>
+          <header>
+            <h1>
+<SubIcon/> pro
+            </h1>
+          </header>
+          <ul>
+          <li>no ads</li>
+          <li>no ads</li>
+          <li>no ads</li>
+          <li>free prime message</li>
+          </ul>
+          <footer>
+            <h1>$100 <span>/ month</span></h1>
+            <button onClick={()=>toast.error("something went wrong")}>choose</button>
+          </footer>
+        </div>
+
+      </section>
+    </main>
   )
 }
 
